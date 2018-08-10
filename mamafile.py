cd5d127eb86072de0d9bf48706e42e527b6fec6c
Jorma <jorma.rebane@gmail.com>
2018-08-11 02:29:38 +0300

    Added support for Mama build

---
+++ mamafile.py
@@ -0,0 +1,18 @@
+import mama
+
+class OpenBLAS(mama.BuildTarget):
+    def dependencies(self):
+        pass
+
+    def configure(self):
+        self.disable_cxx_compiler()
+        self.add_cmake_options(
+            'NOFORTRAN=TRUE',
+            'BUILD_SHARED_LIBS=FALSE',
+        #    'DYNAMIC_ARCH=TRUE',
+        #    'BUILD_WITHOUT_LAPACK=TRUE',
+        #    'BUILD_RELAPACK=TRUE'
+        )
+        if not self.windows:
+            self.add_cl_flags('-fPIC')
+
